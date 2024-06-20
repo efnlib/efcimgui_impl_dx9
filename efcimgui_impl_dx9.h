4a52b55c6d51efc1ac51a43eb77ac77c65e6acf0
Efgeen <efgeen1998@gmail.com>
2024-06-20 14:09:18 +0200

    Update and rename cimgui_impl_dx9.h to efcimgui_impl_dx9.h

---
+++ efcimgui_impl_dx9.h
@@ -0,0 +1,13 @@
+#ifndef _EFCIMGUI_IMPL_DX9
+#define _EFCIMGUI_IMPL_DX9
+
+#include <efstdbool.h>
+
+bool efcimgui_impl_dx9_init(void *dev);
+void efcimgui_impl_dx9_shutdown(void);
+void efcimgui_impl_new_frame(void);
+void efcimgui_impl_dx9_render_draw_data(void *draw_data);
+bool efcimgui_impl_dx9_create_device_objects(void);
+void efcimgui_impl_dx9_invalidate_device_objects(void);
+
+#endif
